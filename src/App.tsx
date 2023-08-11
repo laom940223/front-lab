615adb0433c2ba325332b8869ba9d8f8a32284bb
Manuel Lara <smazzbass@gmail.com>
2023-08-10 23:26:00 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import  { Routes, Route } from "react-router-dom"
+
+
+
+
+function App() {
+  
+
+
+
+
+
+  return (
+    <Routes>
+
+
+        <Route path="/login" Component={<Login/>} />
+    
+    
+    </Routes>
+
+
+  )
+}
+
+export default App
